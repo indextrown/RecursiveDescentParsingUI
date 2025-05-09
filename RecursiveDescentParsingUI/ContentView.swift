8b70f553b096b680131e86b2e595ccc7925c12cd
indextrown <indextrown@gmail.com>
2025-05-09 13:28:36 +0900

    Initial Commit

---
+++ RecursiveDescentParsingUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RecursiveDescentParsingUI
+//
+//  Created by 김동현 on 5/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RecursiveDescentParsingUI/RecursiveDescentParsingUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecursiveDescentParsingUIApp.swift
+//  RecursiveDescentParsingUI
+//
+//  Created by 김동현 on 5/9/25.
+//
+
+import SwiftUI
+
+@main
+struct RecursiveDescentParsingUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
